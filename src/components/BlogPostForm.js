c3efb0281c9b6d3a8ffd72d2c9772c10628d4f2a
Panagiotis Doukas <doukas@cloudevo.ai>
2023-04-03 18:31:54 +0300

    Modify the BlogPostForm component to accept some default props

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
-const BlogPostForm = ({onSubmit}) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+const BlogPostForm = ({onSubmit, initialValues}) => {
+  const [title, setTitle] = useState(initialValues.title);
+  const [content, setContent] = useState(initialValues.content);
 
   return (
     <View>
@@ -25,6 +25,25 @@ const BlogPostForm = ({onSubmit}) => {
       />
     </View>
   );
+};
+
+/* When I use the 'BlogPostForm' component from the 'EditScreen' I need
+   to pass it the 'initialValues' prop which contains an object with 
+   the title and the content of the blogpost I want to change. 
+   But this is not the case when I use the 'BlogPostForm' from the 
+   'CreateScreen'. In this case I do not want to pass some initial value.
+   The problem is that if I do not pass something, 
+   the value in the place of 'initialValues' will be 'undefined', 
+   which will cause an error. 
+   In order to avoid this I can use the 'BlogPostForm.defaultProps' and 
+   give the default values I want the 'initialValues' prop to have,
+   in case I will not pass something to it.
+*/
+BlogPostForm.defaultProps = {
+  initialValues: {
+    title: '',
+    content: ''
+  }
 };
 
 const styles = StyleSheet.create({
